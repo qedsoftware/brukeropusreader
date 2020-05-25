3f65d524661ea5a478593e0d1ece27bf03b49cf7
Christian Fernandez <christian.fernandez@ensicaen.fr>
2020-05-25 19:44:26 +0200

    DEV: modified setup to use setup tools

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from distutils.core import setup
-
+from setuptools import setup
 
 setup(
     name="brukeropusreader",
-    version="1.3.4",
+    use_scm_version=True,
     description="Bruker OPUS File Reader",
     author="QED",
     author_email="brukeropusreader-dev@qed.ai",
     packages=["brukeropusreader"],
-    install_requires=["numpy>=1.13.3", "scipy>=0.19.1"],
+    # install_requires=["numpy>=1.13.3", "scipy>=0.19.1"],
     license="GPLv3",
     url="https://github.com/qedsoftware/brukeropusreader",
 )
