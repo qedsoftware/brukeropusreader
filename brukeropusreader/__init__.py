d4c9dbb4986cf5b34f15158d346623cc7c646de6
ChrisHodgesUK <code@c-hodges.co.uk>
2021-07-26 12:21:03 +0100

    Add support for time-resolved multi-spectrum blocks

--- brukeropusreader/__init__.py
+++ brukeropusreader/__init__.py
@@ -1,7 +1,7 @@
-from .opus_parser import read_file
+from .opus_parser import read_file,parse_sm
 from .opus_data import OpusData
 
 name = "brukeropusreader"
 
-__all__ = ["read_file", "OpusData"]
+__all__ = ["read_file", "OpusData", "parse_sm"]
 __version__ = "1.3"
---
+++ brukeropusreader/__init__.py~
@@ -0,0 +1,7 @@
+from .opus_parser import read_file
+from .opus_data import OpusData
+
+name = "brukeropusreader"
+
+__all__ = ["read_file", "OpusData"]
+__version__ = "1.3"
